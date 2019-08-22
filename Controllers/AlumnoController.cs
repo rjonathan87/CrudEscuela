4f362eb7f718c260e320021482288b7ea656f354
xxxxxxx <rjonathan87@gmail.com>
2019-08-22 15:14:11 -0500

    AddEditViewController

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -28,5 +28,81 @@ namespace ex.Controllers
             return View(await Alumno.ToListAsync());
         }
 
+        // GET vista para crear usuario 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Edad,Matricula,CorreoElectronico")] Alumno alumno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(alumno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(alumno);
+        }
+
+        // GET: CatAseguradoras/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alumno = await _context.Alumnos.FindAsync(id);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+            return View(alumno);
+        }
+
+        // POST: CatAseguradoras/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Edad,Matricula,CorreoElectronico")] Alumno alumno)
+        {
+            if (id != alumno.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(alumno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlumnoExists(alumno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(alumno);
+        }
+
+
+        private bool AlumnoExists(int id)
+        {
+            return _context.Alumnos.Any(e => e.Id == id);
+        }
+
     }
 }
